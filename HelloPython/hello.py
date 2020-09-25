78a76b7ff22a27bae4ac37556b27d82dafe351f3
UFUK AYDAN <69341397+aydanufuk@users.noreply.github.com>
2020-09-25 15:16:11 +0300

    Add files via upload

---
+++ HelloPython/hello.py
@@ -0,0 +1,3 @@
+print("Python is easy hello world!!!")
+"""this is the first python program
+ hello world!"""
---
+++ HelloPython/string.py
@@ -0,0 +1,19 @@
+s="you are awesome"
+print(s)
+
+s1="""you are the 
+creator of 
+the destiny..."""
+print(s1)
+
+print(s[1])
+print(s*2)
+
+print(len(s1))
+
+print(s[0:4])
+print(s[0:])
+print(s[:8])
+
+print(s[0:9:2])
+print(s[15::-1])
