6a48e5a48aab92d52be2def712f6d9f25083975d
UFUK AYDAN <69341397+aydanufuk@users.noreply.github.com>
2020-09-25 21:02:53 +0300

    Add files via upload

---
+++ HelloPython/basic_programs/numbers/averageNumbers.py
@@ -0,0 +1,3 @@
+a,b,c=[int(x) for x in input("enter three integer numbers").split()]
+average = (a+b+c)/3
+print("average of the three numbers is: ",average)
---
+++ HelloPython/basic_programs/student/studentDetails.py
@@ -0,0 +1,5 @@
+studentid = int(input("enter student id"))
+name = input("enter student name")
+marks = float(input("enter marks"))
+
+print("ID: ",studentid,"Name: ",name,"Marks: ",marks)
